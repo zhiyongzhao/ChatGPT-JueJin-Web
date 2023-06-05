4d8ff2a196f3de34bad01276f9dd483371d639df
IDwyh <wyh16619971230@163.com>
2023-06-05 10:36:27 +0800

    修复打包问题

--- src/views/chat/components/Message/Avatar.vue
+++ src/views/chat/components/Message/Avatar.vue
@@ -3,22 +3,28 @@ import { computed } from 'vue'
 import { NAvatar } from 'naive-ui'
 import { useUserStore } from '@/store'
 import { isString } from '@/utils/is'
-import defaultAvatar from '@/assets/avatar.png'
+
+defineProps<Props>()
+
+const defaultAvatar = new URL('@/assets/avatar.png', import.meta.url).href
 
 interface Props {
   image?: boolean
 }
-
-defineProps<Props>()
 
 const userStore = useUserStore()
 const avatar = computed(() => userStore.userInfo.avatar)
 </script>
 
 <template>
-  <template v-if="image">
-    <NAvatar v-if="isString(avatar) && avatar.length > 0" :fallback-src="defaultAvatar" :src="avatar" />
-    <NAvatar v-else :src="defaultAvatar" round />
-  </template>
-  <img v-else src="src/assets/logo.png">
+  <div>
+    <template v-if="image">
+      <NAvatar
+        v-if="isString(avatar) && avatar.length > 0" :fallback-src="defaultAvatar"
+        :src="avatar"
+      />
+      <NAvatar v-else :src="defaultAvatar" round />
+    </template>
+    <img v-else src="@/assets/logo.png">
+  </div>
 </template>
